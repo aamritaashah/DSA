6279f961a5497818a0e4294b52bb8cd33499f60e
aamritaashah <aamritaashah@gmail.com>
2026-09-23 08:23:19 +0530

    Add files via upload

---
+++ array as ar.py
@@ -0,0 +1,13 @@
+'''import array as ar
+a=ar.array('i',[10,20,30,40,50])
+for i in range(5):
+    print(a[i])'''
+
+
+ #from array import *
+
+from array import *
+a = array('i', [10, 20, 30, 40, 50])
+for i in range(5):
+    print(a[i])
+
